50b5dff07470b6d1a653b59ec65367e16135bb12
DogukanGun <dogukangundogan5@gmail.com>
2025-06-28 14:25:07 +0200

    add cors

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import image_route, video_router
 from dotenv import load_dotenv
+
 app = FastAPI()
 load_dotenv()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/")
 async def root():
